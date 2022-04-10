3c3a830e7b3792bc0dd1c16c733bcb1801a232e4
aabom <tobias.abom@gmail.com>
2022-04-10 10:19:52 +0200

    List of expenses now show items based on year

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import React, { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Breakfast",
+    amount: "90",
+    date: new Date(2022, 0, 30),
+  },
+  {
+    id: "e2",
+    title: "Lunch",
+    amount: "110",
+    date: new Date(2022, 1, 30),
+  },
+  {
+    id: "e3",
+    title: "Dinner",
+    amount: "130",
+    date: new Date(2022, 2, 30),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Breakfast",
-      amount: "90",
-      date: new Date(2022, 0, 30),
-    },
-    {
-      id: "e2",
-      title: "Lunch",
-      amount: "110",
-      date: new Date(2022, 1, 30),
-    },
-    {
-      id: "e3",
-      title: "Dinner",
-      amount: "130",
-      date: new Date(2022, 2, 30),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("in app.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
+
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses item={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import ExpensesFilter from "./ExpenseFilter";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import React, { useState } from "react";
 
@@ -11,28 +11,27 @@ function Expenses(props) {
     setFilteredYear(filteredYear);
   };
 
+  const filteredItems = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  console.log(filteredItems);
+
   return (
     <div>
-      <Card>
+      <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onFilteredYear={FilterHandler}
         />
-        <ExpenseItem
-          title={props.item[0].title}
-          amount={props.item[0].amount}
-          date={props.item[0].date}
-        />
-        <ExpenseItem
-          title={props.item[1].title}
-          amount={props.item[1].amount}
-          date={props.item[1].date}
-        />
-        <ExpenseItem
-          title={props.item[2].title}
-          amount={props.item[2].amount}
-          date={props.item[2].date}
-        />
+        {filteredItems.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
